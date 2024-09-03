f5ec217a75022890978929b8688d504c8ded1699
Ilias Fhail <FhailI@StPaulsSchool.org.uk>
2024-09-03 11:47:56 +0100

    Add project files.

---
+++ NEA first draft/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NEA_first_draft
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
