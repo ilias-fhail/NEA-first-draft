f08fc40082f5b56149cc5b39fefdafd8022792e6
Ilias Fhail <FhailI@StPaulsSchool.org.uk>
2025-01-27 22:34:35 +0000

    fixed unit tests to also take return values

--- UnitTests/Calculation Tests.cs
+++ UnitTests/Calculation Tests.cs
@@ -16,56 +16,87 @@ namespace UnitTests
             Assert.AreEqual(0, variance);
         }
 
-        [TestMethod]
-        public void StockVariance_WithMultipleValues()
+        [TestClass]
+        public class RiskCalculatorTests
         {
-            var riskCalculator = new RiskCalculator();
-            var closingPrices = new List<decimal> { 100m, 200m, 300m };
+            [TestMethod]
+            public void StockVariance_WithMultipleValues()
+            {
+                var riskCalculator = new RiskCalculator();
+                var closingPrices = new List<decimal> { 100m, 200m, 300m };
 
-            var variance = riskCalculator.StockVariance(closingPrices);
+                var variance = riskCalculator.StockVariance(closingPrices);
 
-            var expectedVariance = (decimal)((Math.Pow(100 - 200, 2) + Math.Pow(200 - 200, 2) + Math.Pow(300 - 200, 2)) / 2); 
-            Assert.AreEqual(expectedVariance, variance);
+                var returns = new List<decimal>{(200m - 100m) / 100m, (300m - 200m) / 200m};
+
+                var averageReturn = returns.Average();
+                var expectedVariance = (decimal)((Math.Pow((double)(returns[0] - averageReturn), 2) +
+                                                  Math.Pow((double)(returns[1] - averageReturn), 2)) / (returns.Count - 1));
+
+                Assert.AreEqual(expectedVariance, variance);
+            }
+
+            [TestMethod]
+            public void StockCovariance_WithIdenticalValues()
+            {
+                var riskCalculator = new RiskCalculator();
+                var prices1 = new List<decimal> { 100m, 200m, 300m };
+                var prices2 = new List<decimal> { 100m, 200m, 300m };
+
+                var covariance = riskCalculator.StockCovariance(prices1, prices2);
+
+                var returns1 = new List<decimal>{(200m - 100m) / 100m, (300m - 200m) / 200m };
+
+                var returns2 = new List<decimal>{(200m - 100m) / 100m,(300m - 200m) / 200m};
+
+                var average1 = returns1.Average();
+                var average2 = returns2.Average();
+                var expectedCovariance = (decimal)(((returns1[0] - average1) * (returns2[0] - average2) +
+                                                    (returns1[1] - average1) * (returns2[1] - average2)) / (returns1.Count - 1));
+
+                Assert.AreEqual(expectedCovariance, covariance);
+            }
+
+            [TestMethod]
+            public void StockCovariance_WithDifferentValues()
+            {
+                var riskCalculator = new RiskCalculator();
+                var prices1 = new List<decimal> { 100m, 200m, 300m };
+                var prices2 = new List<decimal> { 300m, 200m, 100m };
+
+                var covariance = riskCalculator.StockCovariance(prices1, prices2);
+
+                var returns1 = new List<decimal>{(200m - 100m) / 100m,(300m - 200m) / 200m};
+
+                var returns2 = new List<decimal>{(200m - 300m) / 300m,(100m - 200m) / 200m};
+                var average1 = returns1.Average();
+                var average2 = returns2.Average();
+                var expectedCovariance = ((returns1[0] - average1) * (returns2[0] - average2) +(returns1[1] - average1) * (returns2[1] - average2)) / (returns1.Count - 1);
+
+                Assert.AreEqual(expectedCovariance, covariance);
+            }
+
+            [TestMethod]
+            public void StockCovariance_WithMismatchedLengths()
+            {
+                var riskCalculator = new RiskCalculator();
+                var prices1 = new List<decimal> { 100m, 200m };
+                var prices2 = new List<decimal> { 300m, 200m, 100m };
+
+                var covariance = riskCalculator.StockCovariance(prices1, prices2);
+
+                var returns1 = new List<decimal>{(200m - 100m) / 100m};
+
+                var returns2 = new List<decimal>{(200m - 300m) / 300m };
+
+                var average1 = returns1.Average();
+                var average2 = returns2.Average();
+                var expectedCovariance = (returns1[0] - average1) * (returns2[0] - average2) / (returns1.Count);
+
+                Assert.AreEqual(expectedCovariance, covariance);
+            }
         }
 
-        [TestMethod]
-        public void StockCovariance_WithIdenticalValues()
-        {
-            var riskCalculator = new RiskCalculator();
-            var prices1 = new List<decimal> { 100m, 200m, 300m };
-            var prices2 = new List<decimal> { 100m, 200m, 300m };
-
-            var covariance = riskCalculator.StockCovariance(prices1, prices2);
-
-            var expectedCovariance = (decimal)((Math.Pow(100 - 200, 2) + Math.Pow(200 - 200, 2) + Math.Pow(300 - 200, 2)) / 2);
-            Assert.AreEqual(expectedCovariance, covariance);
-        }
-
-        [TestMethod]
-        public void StockCovariance_WithDifferentValues()
-        {
-            var riskCalculator = new RiskCalculator();
-            var prices1 = new List<decimal> { 100m, 200m, 300m };
-            var prices2 = new List<decimal> { 300m, 200m, 100m };
-
-            var covariance = riskCalculator.StockCovariance(prices1, prices2);
-
-            var expectedCovariance = (decimal)((((100 - 200) * (300 - 200)) + ((200 - 200) * (200 - 200)) + ((300 - 200) * (100 - 200))) / 2);
-            Assert.AreEqual(expectedCovariance, covariance);
-        }
-
-        [TestMethod]
-        public void StockCovariance_WithMismatchedLengths()
-        {
-            var riskCalculator = new RiskCalculator();
-            var prices1 = new List<decimal> { 100m, 200m };
-            var prices2 = new List<decimal> { 300m, 200m, 100m };
-
-            var covariance = riskCalculator.StockCovariance(prices1, prices2);
-
-            var expectedCovariance = (decimal)((((100 - 150) * (300 - 200)) + ((200 - 150) * (200 - 200))) / 1);
-            Assert.AreEqual(expectedCovariance, covariance);
-        }
         [TestMethod]
         public void StockVariance_WithLargeDataSet()
         {
@@ -101,7 +132,7 @@ namespace UnitTests
             }
             var variance = riskCalculator.StockVariance(closeValues);
 
-            var expectedVariance = (decimal)2062.37940;
+            var expectedVariance = (decimal)0.00035;
             Assert.AreEqual(expectedVariance, Math.Round(variance, 5));
         }
         [TestMethod]
@@ -168,7 +199,7 @@ namespace UnitTests
             }
             var Covariance = riskCalculator.StockCovariance(closeValues,closeValues2);
 
-            var expectedVariance = (decimal)-120.02043;
+            var expectedVariance = (decimal)0.00027;
             Assert.AreEqual(expectedVariance, Math.Round(Covariance, 5));
         }
     }
